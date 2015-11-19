b5708ba48a88d438b011738720a81170f4465a10
fatihafizoglu <fatihafizoglu@hotmail.com>
2015-11-19 12:34:09 +0200

    New source based clustering script & execution time log added to random clustering script

---
+++ source_based/createDocIdsUrlsFile.py
@@ -0,0 +1,46 @@
+import sys
+import os
+import re
+import time
+
+def print_usage():
+	print "\nWRONG USAGE:"
+	print "Usage: python sourceBasedClusterBuilder.py [IDP]"
+	print "\t IDP           Input Directory Path"
+	print "Ex:   python createDocIdsUrlsFile.py /media/arcelik/UbuntuExtra/govDatExtracted"
+
+if len(sys.argv) < 2:
+	print_usage()
+	sys.exit()
+
+print "Script started at: " + time.strftime('%X %x')
+
+ARGV_INPUT_DIRECTORY = sys.argv[1]
+
+# Open a file to append urls exctracted from documents
+urls_file = open("urls.txt","w")
+
+for directory_name in os.listdir(ARGV_INPUT_DIRECTORY):
+	for file_name in os.listdir(os.path.join(ARGV_INPUT_DIRECTORY,directory_name)):
+		if ".gz" in file_name: # For skipping zipped files
+			continue
+		
+		gov_dat_partition = open(os.path.join(ARGV_INPUT_DIRECTORY,directory_name,file_name))
+		gov_dat_partition_read = gov_dat_partition.read()
+		gov_dat_partition.close()
+
+		documents = re.findall("<DOC>.*?</DOC>[^\<]",gov_dat_partition_read,re.S)
+
+		for document in documents:
+			
+			doc_id = re.findall("<DOCNO>(.*?)<\/DOCNO>",document)[0]
+			doc_url = re.findall("<DOCHDR>\n.*?([\w]*?\.gov.*)",document)[0]
+
+			urls_file.write(doc_url + " " + doc_id + "\n")
+
+urls_file.close()
+
+# Sort urls.txt file with unix sort
+os.system("sort urls.txt > sorted_urls.txt")
+
+print "Script ended at:   " + time.strftime('%X %x')
--- source_based/sourceBasedClusterBuilder.py
+++ source_based/sourceBasedClusterBuilder.py
@@ -1,22 +1,22 @@
 import sys
 import os
 import re
-import random
 import math
 import json
 import time
 
 def print_usage():
 	print "\nWRONG USAGE:"
-	print "Usage: python randomClusterBuilder.py [IDP] [ODP] [CS] [MS]"
+	print "Usage: python sourceBasedClusterBuilder.py [IDP] [ODP] [NOC] [MS] [NOD]"
 	print "\nArguments"
 	print "\t IDP           Input Directory Path"
 	print "\t ODP           Output Directory Path"
 	print "\t NOC           Number of Clusters"
 	print "\t MS            Maximum number of document in one large file"
-	print "Ex:   python randomClusterBuilder.py /media/arcelik/UbuntuExtra/govDatExtracted /media/arcelik/UbuntuExtra/RandomClusters 50 1000"
+	print "\t NOD           Number of documents in collection"
+	print "Ex:   python sourceBasedClusterBuilder.py /media/arcelik/UbuntuExtra/govDatExtracted /media/arcelik/UbuntuExtra/SourceBasedClusters 50 1000 89771"
 
-if len(sys.argv) < 5:
+if len(sys.argv) < 6:
 	print_usage()
 	sys.exit()
 
@@ -26,6 +26,8 @@ ARGV_INPUT_DIRECTORY = sys.argv[1]
 ARGV_OUTPUT_DIRECTORY = sys.argv[2]
 ARGV_NO_OF_CLUSTER = int(sys.argv[3])
 ARGV_MAX_NO_OF_DOCUMENT_IN_A_FILE = int(sys.argv[4])
+ARGV_NO_OF_DOCUMENTS_IN_COLLECTION = int(sys.argv[5])
+CLUSTER_SIZE = int(math.ceil(float(ARGV_NO_OF_DOCUMENTS_IN_COLLECTION) / ARGV_NO_OF_CLUSTER))
 
 if os.path.exists(ARGV_OUTPUT_DIRECTORY):
 	print "cannot create output directory '" + ARGV_OUTPUT_DIRECTORY + "': Directory exists"
@@ -34,9 +36,18 @@ else:
 	os.mkdir(ARGV_OUTPUT_DIRECTORY)
 	for i in range(ARGV_NO_OF_CLUSTER):
 		os.mkdir(os.path.join(ARGV_OUTPUT_DIRECTORY,str(i)))
-	
+
 cluster_document_dict = {}
 cluster_size_dict = {}
+index = 0
+# Open sorted_urls.txt which is one of the outputs of phase1 - create_urls_files
+with open("sorted_urls.txt") as f:
+	for url_docid in f:
+		if url_docid == "":
+			continue
+		doc_id = url_docid.split("\n")[0].split(" ")[1]
+		cluster_document_dict[doc_id] = index / CLUSTER_SIZE
+		index = index + 1
 
 for directory_name in os.listdir(ARGV_INPUT_DIRECTORY):
 	for file_name in os.listdir(os.path.join(ARGV_INPUT_DIRECTORY,directory_name)):
@@ -50,20 +61,18 @@ for directory_name in os.listdir(ARGV_INPUT_DIRECTORY):
 		documents = re.findall("<DOC>.*?</DOC>[^\<]",gov_dat_partition_read,re.S)
 
 		for document in documents:
-			
-			random_cluster_id = random.randint(0,ARGV_NO_OF_CLUSTER - 1)
-			output_file_name = 0
-
-			if cluster_size_dict.has_key(random_cluster_id):
-				output_file_name = cluster_size_dict[random_cluster_id] / ARGV_MAX_NO_OF_DOCUMENT_IN_A_FILE
-				cluster_size_dict[random_cluster_id] = cluster_size_dict[random_cluster_id] + 1
-			else:
-				cluster_size_dict[random_cluster_id] = 1
 
 			doc_id = re.findall("<DOCNO>(.*?)<\/DOCNO>",document)[0]
-			cluster_document_dict[doc_id] = random_cluster_id
+			cluster_id = cluster_document_dict[doc_id]
+			output_file_name = 0
 
-			cluster_documents_file = open(os.path.join(ARGV_OUTPUT_DIRECTORY,str(random_cluster_id),str(output_file_name)),"a")
+			if cluster_size_dict.has_key(cluster_id):
+				output_file_name = cluster_size_dict[cluster_id] / ARGV_MAX_NO_OF_DOCUMENT_IN_A_FILE
+				cluster_size_dict[cluster_id] = cluster_size_dict[cluster_id] + 1
+			else:
+				cluster_size_dict[cluster_id] = 1
+
+			cluster_documents_file = open(os.path.join(ARGV_OUTPUT_DIRECTORY,str(cluster_id),str(output_file_name)),"a")
 			cluster_documents_file.write(document)
 			cluster_documents_file.close()
 			# TODO: Dosyalari acip acip kapatmaya gerek yok. Max no of file a erisene kadar acik kalabilir. Performance Improvement if needed
