c569611e2ed4eb4030250de37abcb915f4df2fa1
fatihafizoglu <fatihafizoglu@gmail.com>
2016-03-24 16:12:37 +0200

    Comments added+Minor bug before last write fixed+one dimensional doc_id_cluster_id_array instead of multidimensional doc_ids array added

--- index_splitter/index_splitter.py.py
+++ index_splitter/index_splitter.py.py
@@ -14,20 +14,22 @@ word_list_file							= "/home1/sengor/CLUEWEB/createIIS/merged_wordlist.txt"
 
 print "Script Started at: " + time.strftime('%X %x')
 
-# WordList File iterator - Yields word and posting list length of word
+# #
+# This function is an iterator: Yields (term,posting_list_length) each time next called.
 def next_word_posting_list_length():
 	with open(word_list_file) as infile:
 		for line in infile:
 			line_splitted = line.split(" ")
 			yield (line_splitted[0],int(line_splitted[1]))
 
-# Index File iterator - Yields doc_id,occurance_count and byte string to write clusters file
+# #
+# This function is an iterator: Reads index file as chunks and yields (doc_id,occurance_count,8byte<doc_id-occurance_count>) each time next called.
 def bytes_from_file(filename, chunksize=8192):
 	with open(filename, "rb") as f:
 		while True:
 			chunk = f.read(chunksize)
 			# print pympler.asizeof.asizeof(chunk)
-			print "New 1GB just read!"
+			print "New chunk just read!: " + str(chunksize)
 			if chunk:
 				byte_string = ""
 				for byte in chunk:
@@ -40,22 +42,23 @@ def bytes_from_file(filename, chunksize=8192):
 			else:
 				break
 
+# #
+# This function returns an array: Document having id X has belong to cluster at (X-1)th index of returned array.
 def read_cluster_concat():
 	
 	cluster_concat_file = open(os.path.join(clusters_directory,"read","final","cluster_concat_sorted")) 
 	cluster_concat_file_read = cluster_concat_file.readlines()
 	cluster_concat_file.close()
 
-	to_return = [[],[]]
+	to_return = []
 	for index in xrange(len(cluster_concat_file_read)):
 		line_splitted = cluster_concat_file_read[index].split(" ")
-		to_return[0].append(int(line_splitted[0]))
-		to_return[1].append(int(line_splitted[1]))
+		to_return.append(int(line_splitted[1]))
 
 	return to_return
 
 
-doc_ids = read_cluster_concat()
+doc_id_cluster_id_array = read_cluster_concat()
 next_word_posting_list_length_getter = next_word_posting_list_length()
 
 cluster_binary_files = []
@@ -80,9 +83,10 @@ for (doc_id,occurance_count,byte_string) in bytes_from_file(index_file,10*1024*1
 	
 	if posting_list_length == 0:
 		for i in xrange(__NO_OF_CLUSTERS__):
-			if cluster_word_occurances[i] > 0:
-				cluster_word_lists[i].append(latest_word + " " + str(cluster_word_occurances[i]) + "\n")
-				cluster_word_occurances[i] = 0
+			# if cluster_word_occurances[i] > 0:
+			# Non existing words will be on wordlist file with no of occurance as 0
+			cluster_word_lists[i].append(latest_word + " " + str(cluster_word_occurances[i]) + " -1" + "\n")
+			cluster_word_occurances[i] = 0
 
 		if total_size_of_posting_lists_in_memory * 8 > 5*1024*1024*1024: # Larger than 5GB data in memory!
 			print "Writing Started"
@@ -106,12 +110,15 @@ for (doc_id,occurance_count,byte_string) in bytes_from_file(index_file,10*1024*1
 
 	posting_list_length = posting_list_length - 1
 
-	cluster_id = doc_ids[1][doc_id - 1]
+	cluster_id = doc_id_cluster_id_array[doc_id - 1]
 
 	cluster_binaries[cluster_id].append(byte_string)
 	cluster_word_occurances[cluster_id] += 1
 
-
+for i in xrange(__NO_OF_CLUSTERS__):
+	# if cluster_word_occurances[i] > 0:
+	# Non existing words will be on wordlist file with no of occurance as 0
+	cluster_word_lists[i].append(latest_word + " " + str(cluster_word_occurances[i]) + " -1" + "\n")
 print "Last Writing Started"
 for i in xrange(__NO_OF_CLUSTERS__):
 	for binary_string in cluster_binaries[i]:
