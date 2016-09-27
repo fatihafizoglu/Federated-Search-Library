9a304070a7993637f903ca5d5e0c3359715c1d4a
Emre Can Kucukoglu <eckucukoglu@gmail.com>
2016-09-28 01:33:51 +0300

    main program runs for each div algos/lambdas.

--- diversify/c/main.c
+++ diversify/c/main.c
@@ -4,15 +4,20 @@
 int main (int argc, char *argv[]) {
     char wordlist_path[FILEPATH_LENGTH] = "/home/eckucukoglu/projects/ms-thesis/allocation_runs/topic_based_2/csi_wordlist_idf.txt";
     char document_info_path[FILEPATH_LENGTH] = "/home/eckucukoglu/projects/ms-thesis/main_index/doc_lengths.txt";
-    char document_vectors_folder_path[FILEPATH_LENGTH] = "/home/eckucukoglu/projects/ms-thesis/main_index/doc_vectors";
-    char preresults_path[FILEPATH_LENGTH] = "/home/eckucukoglu/projects/ms-thesis/results/3.txt";
+    char document_vectors_folder_path[FILEPATH_LENGTH] = "/home/eckucukoglu/projects/ms-thesis/main_index/dvec.bins";
+    char preresults_path[FILEPATH_LENGTH] = "/home/eckucukoglu/projects/ms-thesis/results/run.txt";
     unsigned int number_of_documents = 50220538;
     unsigned int number_of_terms = 163629158;
     unsigned int number_of_preresults = 200;
     unsigned int number_of_results = 100;
     unsigned int number_of_query = 50;
-    unsigned int diversification_algorithm = MAX_SUM;
-
+    unsigned int div_algorithms[] = {MAX_SUM, SF};
+    double div_lambdas[] = { 0.25, 0.5, 0.75, 1 };
+    
+    int i, j;
+    int div_len = sizeof(div_algorithms) / sizeof(unsigned int);
+    int lambda_len = sizeof(div_lambdas) / sizeof(double);
+    
     Conf conf = {
         .wordlist_path = wordlist_path,
         .document_info_path = document_info_path,
@@ -24,20 +29,27 @@ int main (int argc, char *argv[]) {
         .DIVERSIFY = true,
         .number_of_preresults = number_of_preresults,
         .number_of_results = number_of_results,
-        .number_of_query = number_of_query,
-        .diversification_algorithm = diversification_algorithm
+        .number_of_query = number_of_query
     };
 
-    {
-        initDiversify(&conf);
-        openDocumentVectorsFiles();
-        loadDocuments();
-        loadTerms();
-        loadPreresults();
-        diversify();
-        writeResults();
-        endProgram ();
+    for (i = 0; i < div_len; i++) {
+        for (j = 0; j < lambda_len; j++) {
+            conf.diversification_algorithm = div_algorithms[i];
+            conf.lambda = div_lambdas[j];
+            
+            {
+                initDiversify(&conf);
+                openDocumentVectorsFiles();
+                loadDocuments();
+                loadTerms();
+                loadPreresults();
+                diversify();
+                writeResults();
+                endProgram ();
+            }
+            
+        }   
     }
-
+        
     return 0;
 }
