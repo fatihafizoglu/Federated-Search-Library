397e1e8021bf3400aa2bd8846c05e8a81aafac90
Emre Can Kucukoglu <eckucukoglu@gmail.com>
2016-09-28 04:35:36 +0300

    now upper limit can be limited.

--- result_rank_fixer/result_rank_fixer.c
+++ result_rank_fixer/result_rank_fixer.c
@@ -7,7 +7,7 @@ void writeResults () {
     char results_path[FILEPATH_LENGTH] = "";
     size_t len1 = strlen(config->results_path);
     memcpy(results_path, config->results_path, len1);
-    memcpy(results_path+len1, "_sorted", 7);
+    memcpy(results_path+len1, "_fixed", 6);
 
     if (!(fp = fopen(results_path, "w"))) {
         return;
@@ -15,7 +15,7 @@ void writeResults () {
 
     for (q_no = 0; q_no < config->number_of_query; q_no++) {
         rank = 1;
-        for (j = 0; j < config->number_of_results; j++) {
+        for (j = 0; (j < config->number_of_preresults) && (rank <= config->number_of_results); j++) {
             if (results[q_no][j].doc_id != 0 && results[q_no][j].score != 0) {
                 fprintf(fp, "%d\tQ0\t%d\t%d\t%lf\tfs\n", q_no + 1, results[q_no][j].doc_id, rank++, results[q_no][j].score);
             }
@@ -59,7 +59,7 @@ int initFixer (Conf *conf) {
 
     int i, j;
     long results_pointer_alloc_size = config->number_of_query * sizeof(Result *);
-    long results_alloc_size = config->number_of_results * sizeof(Result);
+    long results_alloc_size = config->number_of_preresults * sizeof(Result);
 
     if (!(results = malloc(results_pointer_alloc_size))) {
         return -1;
@@ -72,7 +72,7 @@ int initFixer (Conf *conf) {
     }
 
     for (i = 0; i < config->number_of_query; i++) {
-        for (j = 0; j < config->number_of_results; j++) {
+        for (j = 0; j < config->number_of_preresults; j++) {
             results[i][j].doc_id = 0;
             results[i][j].score = 0;
         }
@@ -83,11 +83,13 @@ int initFixer (Conf *conf) {
 }
 
 int main (int argc, char *argv[]) {
-    unsigned int number_of_results = 500;
+    unsigned int number_of_preresults = 500;
+    unsigned int number_of_results = 200;
     unsigned int number_of_query = 50;
     char results_path[FILEPATH_LENGTH] = "/home/eckucukoglu/Dropbox/arcelik/arcelikTeam/results_nospam/c500ns";
 
     Conf conf = {
+        .number_of_preresults = number_of_preresults,
         .number_of_results = number_of_results,
         .number_of_query = number_of_query,
         .results_path = results_path
