47c952f7832f7bc4106018f36f6cf8f65fbc335f
fatihafizoglu <fatihafizoglu@hotmail.com>
2016-10-24 12:14:12 +0300

    3_stat_producer.c - First 2 metric completed

--- query_processor_python/3_stat_producer.c
+++ query_processor_python/3_stat_producer.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define DOC_ID_TERM_FREQUENCY_PAIR_SIZE_IN_BYTES 8
 #define DOC_NO 50220539
 #define NO_OF_QUERY 50
 #define NO_OF_CLUSTERS 100
-#define QUERY_FILE_PATH "queries_MAIN.txt"
-#define SELECTED_RESOURCES_FILE_PATH "selected_resources_TOPIC_CSI.txt"
+#define QUERY_FILE_PATH "2009_queries_MAIN.txt"
+#define SELECTED_RESOURCES_FILE_PATH "2009_selected_resources_TOPIC_CSI.txt"
 #define TOPIC_DOCUMENT_CLUSTER_MAP_FILE_PATH "/media/fatihafizoglu/LenovoMS/MS/TopicBasedClusters_100_2/read/final/cluster_concat_sorted"
 #define RANDOM_DOCUMENT_CLUSTER_MAP_FILE_PATH "/media/fatihafizoglu/LenovoMS/MS/RandomBasedClusters_100/read/final/cluster_concat_sorted"
 #define INDEX_FILE_PATH "/media/fatihafizoglu/LenovoMS/MS/Index/merged_entry.txt"
@@ -30,6 +31,18 @@ unsigned int *selected_resources_lengths;
 unsigned int *topic_document_cluster_map;
 unsigned int *random_document_cluster_map;
 
+void print_time(char *explanation) {
+    time_t timer;
+    char buffer[26];
+    struct tm* tm_info;
+
+    time(&timer);
+    tm_info = localtime(&timer);
+
+    strftime(buffer, 26, "%H:%M:%S %m/%d/%y", tm_info);
+    printf("%s %s\n", explanation, buffer);
+}
+
 void read_queries() {
     FILE *fp;
     unsigned int query_index = 0;
@@ -45,7 +58,7 @@ void read_queries() {
         
         queries[query_index].terms = malloc(queries[query_index].queryLength * sizeof(term));
 
-        for ( i = 0; i < queries[query_index].queryLength; i++ ) {
+        for (i = 0; i < queries[query_index].queryLength; i++) {
             fscanf(fp,"%u", &queries[query_index].terms[i].occurange_in_query);
             fscanf(fp,"%u %lf %lu", &queries[query_index].terms[i].occurange_in_docs, &queries[query_index].terms[i].cfc_weight, &queries[query_index].terms[i].disk_address);
         }
@@ -112,18 +125,22 @@ void read_cluster_map(unsigned int *map, char *map_path) {
 void main(void) {
     
     FILE *fp;
+    FILE **csvs;
     unsigned int query_index, i, j, k;
     unsigned int total_posting_list_checked;
     unsigned int max_posting_list_checked;
     unsigned int *posting_list;
     unsigned int *document_accesses;
-    unsigned int **cluster_accesses;
     unsigned int topic_cluster_id;
     unsigned int random_cluster_id;
 
 
-    // // Variables that keeps statistics
-    // unsigned int **posting_list_access_statistics_sum;
+    // Variables that keeps statistics
+    unsigned int ***cluster_accesses;
+    unsigned int **total_posting_lists;
+    unsigned int **max_posting_lists;
+
+    print_time("Script Started at:");
 
     queries = (query*) malloc(NO_OF_QUERY * sizeof(query));
     selected_resources = (unsigned int**) malloc(NO_OF_QUERY * sizeof(unsigned int*));
@@ -136,26 +153,38 @@ void main(void) {
     read_cluster_map(topic_document_cluster_map, TOPIC_DOCUMENT_CLUSTER_MAP_FILE_PATH);
     read_cluster_map(random_document_cluster_map, RANDOM_DOCUMENT_CLUSTER_MAP_FILE_PATH);
 
-    // // Init variables that keeps statistics
-    // posting_list_access_statistics_sum = (unsigned int**) malloc(NO_OF_QUERY * sizeof(unsigned int*));
-    // for ( query_index = 0; query_index < NO_OF_QUERY; query_index++) {
-    //     posting_list_access_statistics_sum[query_index] = (unsigned int*) malloc(NO_OF_METHOD * sizeof(unsigned int));
-    //     for ( i = 0; i < NO_OF_METHOD; i++) {
-    //         posting_list_access_statistics_sum[query_index][i] = 0;
-    //     }
-    // }
+    // Init variables that keeps statistics
+    csvs = (FILE**) malloc(NO_OF_METHOD * sizeof(FILE*));
 
+    cluster_accesses = (unsigned int***) malloc(NO_OF_QUERY * sizeof(unsigned int**));
+    total_posting_lists = (unsigned int**) malloc(NO_OF_QUERY * sizeof(unsigned int*));
+    max_posting_lists = (unsigned int**) malloc(NO_OF_QUERY * sizeof(unsigned int*));
+    for (query_index = 0; query_index < NO_OF_QUERY; query_index++) {
+        cluster_accesses[query_index] = (unsigned int**) malloc(NO_OF_METHOD * sizeof(unsigned int*));
+        total_posting_lists[query_index] = (unsigned int*) malloc(NO_OF_METHOD * sizeof(unsigned int));
+        max_posting_lists[query_index] = (unsigned int*) malloc(NO_OF_METHOD * sizeof(unsigned int));
+        for (i = 0; i < NO_OF_METHOD; i++) {
+            cluster_accesses[query_index][i] = (unsigned int*) malloc(NO_OF_CLUSTERS * sizeof(unsigned int));
+            total_posting_lists[query_index][i] = 0;
+            max_posting_lists[query_index][i] = 0;
+            for (j = 0; j < NO_OF_CLUSTERS; j++) {
+                cluster_accesses[query_index][i][j] = 0;
+            }
+        }
+    }
+
+    print_time("Stats Started at:");
     fp = fopen(INDEX_FILE_PATH, "rb");
-    for ( query_index = 0; query_index < NO_OF_QUERY; query_index++) {
-        // printf("%u\n", query_index + 1);
+    for (query_index = 0; query_index < NO_OF_QUERY; query_index++) {
+        printf("%u\n", query_index + 1);
 
         document_accesses = (unsigned int*) malloc(DOC_NO * sizeof(unsigned int));
-        for ( i = 0; i < DOC_NO; i++) {
+        for (i = 0; i < DOC_NO; i++) {
             document_accesses[i] = 0;
         }
 
-        for ( i = 0; i < queries[query_index].queryLength; i++ ) {
-            posting_list = (unsigned int*) malloc( DOC_ID_TERM_FREQUENCY_PAIR_SIZE_IN_BYTES * queries[query_index].terms[i].occurange_in_docs * sizeof(unsigned int));
+        for (i = 0; i < queries[query_index].queryLength; i++ ) {
+            posting_list = (unsigned int*) malloc(DOC_ID_TERM_FREQUENCY_PAIR_SIZE_IN_BYTES * queries[query_index].terms[i].occurange_in_docs * sizeof(unsigned int));
             fseek(fp, DOC_ID_TERM_FREQUENCY_PAIR_SIZE_IN_BYTES * queries[query_index].terms[i].disk_address, 0);
             fread(posting_list, DOC_ID_TERM_FREQUENCY_PAIR_SIZE_IN_BYTES, queries[query_index].terms[i].occurange_in_docs, fp);
 
@@ -166,55 +195,71 @@ void main(void) {
         }
 
         
-        // Calculate statistics!
-        cluster_accesses = (unsigned int**) malloc(NO_OF_METHOD * sizeof(unsigned int*));
-        for ( i = 0; i < NO_OF_METHOD; i++) {
-            cluster_accesses[i] = (unsigned int*) malloc(NO_OF_CLUSTERS * sizeof(unsigned int));
-            for ( j = 0; j < NO_OF_CLUSTERS; j++) {
-                cluster_accesses[i][j] = 0;
-            }
-        }
-        for ( i = 0; i < DOC_NO; i++) {
-            cluster_accesses[0][0] += document_accesses[i]; // EXHAUSTIVE - Increment first cluster
+        // Calculate statistics new!
+        for (i = 0; i < DOC_NO; i++) {
+            cluster_accesses[query_index][0][0] += document_accesses[i]; // EXHAUSTIVE - Increment first cluster
 
             topic_cluster_id = topic_document_cluster_map[i];
             random_cluster_id = random_document_cluster_map[i];
 
-            for ( j = 0; j < selected_resources_lengths[query_index]; j++) {
+            for (j = 0; j < selected_resources_lengths[query_index]; j++) {
                 if (selected_resources[query_index][j] == topic_cluster_id) {
-                    cluster_accesses[1][topic_cluster_id] += document_accesses[i]; // TOPIC - Increment documents topic cluster
-                    cluster_accesses[2][random_cluster_id] += document_accesses[i]; // RANDOM AND TOPIC - Increment documents topic cluster
+                    cluster_accesses[query_index][1][topic_cluster_id] += document_accesses[i]; // TOPIC - Increment documents topic cluster
+                    cluster_accesses[query_index][2][random_cluster_id] += document_accesses[i]; // RANDOM AND TOPIC - Increment documents topic cluster
                     break;
                 }
             }
 
-            cluster_accesses[3][random_cluster_id] += document_accesses[i]; // RANDOM - Increment documents random cluster
+            cluster_accesses[query_index][3][random_cluster_id] += document_accesses[i]; // RANDOM - Increment documents random cluster
         }
-
-        for ( i = 0; i < NO_OF_METHOD; i++) {
-            total_posting_list_checked = 0;
-            max_posting_list_checked = 0;
-            for ( j = 0; j < NO_OF_CLUSTERS; j++) {
-                total_posting_list_checked += cluster_accesses[i][j];
-                if (cluster_accesses[i][j] > max_posting_list_checked) {
-                    max_posting_list_checked = cluster_accesses[i][j];
-                }
-            }
-            printf("%u\n", max_posting_list_checked);
-            printf("%u\n", total_posting_list_checked);
-            printf("-------\n");
-        }
-        printf("************************************************************************************\n");
-        
-
-        for ( i = 0; i < NO_OF_METHOD; i++) {
-            free(cluster_accesses[i]);
-        }
-        free(cluster_accesses);
 
         free(document_accesses);
     }
     fclose(fp);
 
+    csvs[0] = fopen("stat_EXHAUSTIVE.csv","w");
+    csvs[1] = fopen("stat_TOPIC.csv","w");
+    csvs[2] = fopen("stat_RANDOM_AND_TOPIC.csv","w");
+    csvs[3] = fopen("stat_RANDOM.csv","w");
+    for (query_index = 0; query_index < NO_OF_QUERY; query_index++) {
+        for (i = 0; i < NO_OF_METHOD; i++) {
+            for (j = 0; j < NO_OF_CLUSTERS; j++) {
+                if (j != NO_OF_CLUSTERS - 1)
+                    fprintf(csvs[i], "%u,", cluster_accesses[query_index][i][j]);
+                else
+                    fprintf(csvs[i], "%u\n", cluster_accesses[query_index][i][j]);
+                total_posting_lists[query_index][i] += cluster_accesses[query_index][i][j];
+                if (cluster_accesses[query_index][i][j] > max_posting_lists[query_index][i])
+                    max_posting_lists[query_index][i] = cluster_accesses[query_index][i][j];
+            }
+        }
+    }
+    fclose(csvs[0]);
+    fclose(csvs[1]);
+    fclose(csvs[2]);
+    fclose(csvs[3]);
+
+    printf("********************************************************************************\n");
+    printf("MAX POSTING LIST PROCESSED\n");
+    for (query_index = 0; query_index < NO_OF_QUERY; query_index++) {
+        for (i = 0; i < NO_OF_METHOD; i++) {
+            if (i != NO_OF_METHOD - 1)
+                printf("%u,", max_posting_lists[query_index][i]);
+            else
+                printf("%u\n", max_posting_lists[query_index][i]);
+        }
+    }
+    printf("********************************************************************************\n");
+    printf("TOTAL POSTING LIST PROCESSED\n");
+    for (query_index = 0; query_index < NO_OF_QUERY; query_index++) {
+        for (i = 0; i < NO_OF_METHOD; i++) {
+            if (i != NO_OF_METHOD - 1)
+                printf("%u,", total_posting_lists[query_index][i]);
+            else
+                printf("%u\n", total_posting_lists[query_index][i]);
+        }
+    }
+
+    print_time("Script Ended at:");
     return;
 }
