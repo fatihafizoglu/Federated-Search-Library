2a39365e8cb9c60b305700bb147a4f254324c696
Emre Can Kucukoglu <eckucukoglu@gmail.com>
2019-07-22 21:15:02 +0300

    generate eval for BDIV DDIV BASELINE

--- runner/run.sh
+++ runner/run.sh
@@ -6,58 +6,58 @@
 ##
 ##  What it is?
 ##      - This script has some functionalities to diversify, make resouce
-##        selection and evaluation from query result files. Moreover, 
+##        selection and evaluation from query result files. Moreover,
 ##        evaluation tables can be generated easily.
 ##
 ##  How to run?
-##      - Get diversify(c), resource selection(python) and Trec Web 
+##      - Get diversify(c), resource selection(python) and Trec Web
 ##        Evaluation(c) codes.
 ##      - Build diversify according to reference result numbers below.
 ##      - Build Trec Web Evaluation (ndeval).
-##      - Put diversify, ndeval executables or resource_selection.py file 
-##        on the same folder with this script. 
+##      - Put diversify, ndeval executables or resource_selection.py file
+##        on the same folder with this script.
 ##      - Change the 'paths' -they are not filenames- from below.
 ##      - Comment-out the function from end of code that you want to use.
-##  
+##
 ##  Functions:
 ##      * diversify_CSI             [preresult(200) to result(100)]
 ##      * diversify_Rs              [preresult(100) to result(20)]
 ##      * diversify_DivRs           [preresult(100) to result(20)]
 ##      * resourceSelection_CSI
 ##      * resourceSelection_Div
-##      * evaluate_RsDiv 
+##      * evaluate_RsDiv
 ##      * evaluate_DivRsDiv
 ##      * generateEvalTables_RsDiv
 ##      * generateEvalTables_DivRsDiv
 ##
-##  Note: 
-##        
+##  Note:
+##
 ##
 #############################################################################
 ## PATHS ##
 #############################################################################
-CSI_Top500_NoSpamFixed="c500nsf"
-MAIN_Top100k_NoSpamFixed="m100knsf"
-Doc2Cluster_Map="doc_sorted"
-Ground_Truth="ground_truth-2009-withA.txt"
+CSI_Top200_NoSpamFixed="results/pool/c200"
+MAIN_Top100k_NoSpamFixed="results/m100kns/m100kns_fixed"
+Doc2Cluster_Map="data/doc_to_cluster_map"
+#Ground_Truth="ground_truth-2009-withA.txt"
 #############################################################################
 ## Possible options ##
 #############################################################################
-rsAlgorithms=( 
-    "CRCSExp" 
-    "CRCSLin" 
-    "GAVG" 
-    "Redde" 
+rsAlgorithms=(
+    "CRCSExp"
+    "CRCSLin"
+    "GAVG"
+    "Redde"
     "Redde.top"
 )
-divAlgorithms=( 
-    "maxsum0.25" 
-    "maxsum0.50" 
-    "maxsum0.75" 
+divAlgorithms=(
+    "maxsum0.25"
+    "maxsum0.50"
+    "maxsum0.75"
     "maxsum1.00"
-    "sf0.25" 
-    "sf0.50" 
-    "sf0.75" 
+    "sf0.25"
+    "sf0.50"
+    "sf0.75"
     "sf1.00"
 )
 #############################################################################
@@ -86,19 +86,19 @@ getMergedFilenames() {
     local -a filenames=()
     local -a argArr1=("${!1}")
     local -a argArr2=("${!2}")
-    
+
     for i in ${argArr1[@]}; do
         for j in ${argArr2[@]}; do
             filenames[${#filenames[@]}]="${i}_${j}"
         done
     done
-    
+
     echo ${filenames[@]}
 }
 
 diversifyList() {
     local -a argArr=("${!1}")
-      
+
     for i in ${argArr[@]}; do
        execute "./diversify ${i}"
     done
@@ -109,7 +109,7 @@ resourceSelectList() {
 
     for i in ${argArr[@]}; do
         for alg in ${rsAlgorithms[@]}; do
-            execute "python resource_selection.py ${MAIN_Top100k_NoSpamFixed} ${i} ${Doc2Cluster_Map} -method ${alg}"
+            execute "python rs.py ${MAIN_Top100k_NoSpamFixed} ${i} ${Doc2Cluster_Map} -method ${alg}"
         done
     done
 }
@@ -123,36 +123,36 @@ evaluateList() {
 }
 
 diversify_CSI () {
-    diversifyList CSI_Top500_NoSpamFixed       
+    diversifyList CSI_Top200_NoSpamFixed
 }
 
 diversify_Rs () {
     local -a rsFiles=()
-    rsFiles=$(getMergedFilenames CSI_Top500_NoSpamFixed rsAlgorithms[@])   
+    rsFiles=$(getMergedFilenames CSI_Top200_NoSpamFixed rsAlgorithms[@])
     diversifyList rsFiles[@]
 }
 
 diversify_DivRs () {
     local -a divFiles=()
-    divFiles=$(getMergedFilenames CSI_Top500_NoSpamFixed divAlgorithms[@])
+    divFiles=$(getMergedFilenames CSI_Top200_NoSpamFixed divAlgorithms[@])
     local -a divRsFiles=()
     divRsFiles=$(getMergedFilenames divFiles rsAlgorithms[@])
     diversifyList divRsFiles[@]
 }
 
 resourceSelection_CSI () {
-    resourceSelectList CSI_Top500_NoSpamFixed
+    resourceSelectList CSI_Top200_NoSpamFixed
 }
 
 resourceSelection_Div () {
     local -a divFiles=()
-    divFiles=$(getMergedFilenames CSI_Top500_NoSpamFixed divAlgorithms[@])
+    divFiles=$(getMergedFilenames CSI_Top200_NoSpamFixed divAlgorithms[@])
     resourceSelectList divFiles[@]
 }
 
 evaluate_RsDiv () {
     local -a rsFiles=()
-    divFiles=$(getMergedFilenames CSI_Top500_NoSpamFixed rsAlgorithms[@])
+    divFiles=$(getMergedFilenames CSI_Top200_NoSpamFixed rsAlgorithms[@])
     local -a rsDivFiles=()
     rsDivFiles=$(getMergedFilenames divFiles divAlgorithms[@])
     evaluateList rsDivFiles[@]
@@ -160,7 +160,7 @@ evaluate_RsDiv () {
 
 evaluate_DivRsDiv () {
     local -a divFiles=()
-    divFiles=$(getMergedFilenames CSI_Top500_NoSpamFixed divAlgorithms[@])
+    divFiles=$(getMergedFilenames CSI_Top200_NoSpamFixed divAlgorithms[@])
     local -a divRsFiles=()
     divRsFiles=$(getMergedFilenames divFiles rsAlgorithms[@])
     local -a divRsDivFiles=()
@@ -168,41 +168,55 @@ evaluate_DivRsDiv () {
     evaluateList divRsDivFiles[@]
 }
 
-generateEvalTables_RsDiv () {
+generateEvalTables_BASELINE () {
     local -a rsFiles=()
-    divFiles=$(getMergedFilenames CSI_Top500_NoSpamFixed rsAlgorithms[@])
-    local -a rsDivFiles=()
-    rsDivFiles=$(getMergedFilenames divFiles divAlgorithms[@])
-    local -a rsDivEvalFiles=()
+    rsFiles=$(getMergedFilenames CSI_Top200_NoSpamFixed rsAlgorithms[@])
+    local -a rsEvalFiles=()
     local -a eval_suffix=("eval")
-    rsDivEvalFiles=$(getMergedFilenames rsDivFiles eval_suffix)
-    
-    rsDiv_eval_file="rsDiv_eval"
-    echo "file,runid,topic,ERR-IA@5,ERR-IA@10,ERR-IA@20,nERR-IA@5,nERR-IA@10,nERR-IA@20,alpha-DCG@5,alpha-DCG@10,alpha-DCG@20,alpha-nDCG@5,alpha-nDCG@10,alpha-nDCG@20,NRBP,nNRBP,MAP-IA,P-IA@5,P-IA@10,P-IA@20,strec@5,strec@10,strec@20" > ${rsDiv_eval_file}
-    
-    for i in ${rsDivEvalFiles[@]}; do
-       line=$(tail -1 $i)
-       echo $i,$line >> ${rsDiv_eval_file}
+    rsEvalFiles=$(getMergedFilenames rsFiles eval_suffix)
+
+    rs_eval_file="BASELINE_eval"
+    echo "file,runid,topic,ERR-IA@5,ERR-IA@10,ERR-IA@20,nERR-IA@5,nERR-IA@10,nERR-IA@20,alpha-DCG@5,alpha-DCG@10,alpha-DCG@20,alpha-nDCG@5,alpha-nDCG@10,alpha-nDCG@20,NRBP,nNRBP,MAP-IA,P-IA@5,P-IA@10,P-IA@20,strec@5,strec@10,strec@20" > ${rs_eval_file}
+
+    for i in ${rsEvalFiles[@]}; do
+        line=$(tail -1 $i)
+        echo $i,$line >> ${rs_eval_file}
     done
 }
 
-generateEvalTables_DivRsDiv () {
+generateEvalTables_BDIV () {
+    local -a rsFiles=()
+    rsFiles=$(getMergedFilenames CSI_Top200_NoSpamFixed rsAlgorithms[@])
+    local -a rsDivFiles=()
+    rsDivFiles=$(getMergedFilenames rsFiles divAlgorithms[@])
+    local -a rsDivEvalFiles=()
+    local -a eval_suffix=("eval")
+    rsDivEvalFiles=$(getMergedFilenames rsDivFiles eval_suffix)
+
+    rsDiv_eval_file="BDIV_eval"
+    echo "file,runid,topic,ERR-IA@5,ERR-IA@10,ERR-IA@20,nERR-IA@5,nERR-IA@10,nERR-IA@20,alpha-DCG@5,alpha-DCG@10,alpha-DCG@20,alpha-nDCG@5,alpha-nDCG@10,alpha-nDCG@20,NRBP,nNRBP,MAP-IA,P-IA@5,P-IA@10,P-IA@20,strec@5,strec@10,strec@20" > ${rsDiv_eval_file}
+
+    for i in ${rsDivEvalFiles[@]}; do
+        line=$(tail -1 $i)
+        echo $i,$line >> ${rsDiv_eval_file}
+    done
+}
+
+generateEvalTables_DDIV () {
     local -a divFiles=()
-    divFiles=$(getMergedFilenames CSI_Top500_NoSpamFixed divAlgorithms[@])
+    divFiles=$(getMergedFilenames CSI_Top200_NoSpamFixed divAlgorithms[@])
     local -a divRsFiles=()
     divRsFiles=$(getMergedFilenames divFiles rsAlgorithms[@])
-    local -a divRsDivFiles=()
-    divRsDivFiles=$(getMergedFilenames divRsFiles divAlgorithms[@])
-    local -a divRsDivEvalFiles=()
+    local -a divRsEvalFiles=()
     local -a eval_suffix=("eval")
-    divRsDivEvalFiles=$(getMergedFilenames divRsDivFiles eval_suffix)
-    
-    divRsDiv_eval_file="divRsDiv_eval"
-    echo "file,runid,topic,ERR-IA@5,ERR-IA@10,ERR-IA@20,nERR-IA@5,nERR-IA@10,nERR-IA@20,alpha-DCG@5,alpha-DCG@10,alpha-DCG@20,alpha-nDCG@5,alpha-nDCG@10,alpha-nDCG@20,NRBP,nNRBP,MAP-IA,P-IA@5,P-IA@10,P-IA@20,strec@5,strec@10,strec@20" > ${divRsDiv_eval_file}
-    
-    for i in ${divRsDivEvalFiles[@]}; do
+    divRsEvalFiles=$(getMergedFilenames divRsFiles eval_suffix)
+
+    divRs_eval_file="DDIV_eval"
+    echo "file,runid,topic,ERR-IA@5,ERR-IA@10,ERR-IA@20,nERR-IA@5,nERR-IA@10,nERR-IA@20,alpha-DCG@5,alpha-DCG@10,alpha-DCG@20,alpha-nDCG@5,alpha-nDCG@10,alpha-nDCG@20,NRBP,nNRBP,MAP-IA,P-IA@5,P-IA@10,P-IA@20,strec@5,strec@10,strec@20" > ${divRs_eval_file}
+
+    for i in ${divRsEvalFiles[@]}; do
        line=$(tail -1 $i)
-       echo $i,$line >> ${divRsDiv_eval_file}
+       echo $i,$line >> ${divRs_eval_file}
     done
 }
 
@@ -216,5 +230,6 @@ generateEvalTables_DivRsDiv () {
 #resourceSelection_Div
 #evaluate_RsDiv
 #evaluate_DivRsDiv
-#generateEvalTables_RsDiv
-#generateEvalTables_DivRsDiv
+#generateEvalTables_BASELINE
+#generateEvalTables_BDIV
+#generateEvalTables_DDIV
