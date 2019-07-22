ee7228f3ff8416e52c28d64eae0122cbbd68288e
Emre Can Kucukoglu <eckucukoglu@gmail.com>
2019-07-22 20:15:57 +0200

    add quick eval

---
+++ runner/eval.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## e.g. ./eval.sh results/pool/*
+
+Ground_Truth="/home1/grupef/ecank/data/GT_TREC09101112.txt"
+
+for result in "$@"
+do
+    ./ndeval ${Ground_Truth} ${result} > ${result}_eval
+done
+
+source ./run.sh
+generateEvalTables_BASELINE
+generateEvalTables_BDIV
+generateEvalTables_DDIV
