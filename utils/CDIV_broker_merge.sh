30bf3733057b48cf231d806807427a29768b3f95
Emre Can Kucukoglu <eckucukoglu@gmail.com>
2019-08-03 00:56:10 +0200

    use ^ regexp for linestart

--- utils/CDIV_broker_merge.sh
+++ utils/CDIV_broker_merge.sh
@@ -2,37 +2,34 @@
 
 Q_C_MAP=$1
 
-START=1
-END=1 #198
+START=1 #1
+END=198 #198
 
-div_methods=(maxsum0.25 
-            maxsum0.50 
-            maxsum0.75 
-            sf0.25 
-            sf0.50 
+div_methods=(maxsum0.25
+            maxsum0.50
+            maxsum0.75
+            sf0.25
+            sf0.50
             sf0.75)
 
 for (( i=$START; i<=$END; i++ ))
 do
-    printf "Merging query#${i} results...\n"
-    cs=`pcregrep -M "\n${i} " $Q_C_MAP | cut -f2 -d" "`
+    mkdir c200_rs_cdiv_qs/q${i}
+    echo "Merging query#${i} results...\n"
+    cs=`pcregrep -M "^${i} " $Q_C_MAP | cut -f2 -d" "`
     clusters=($cs)
-    printf "\tCollecting "
-    for cluster in ${clusters[@]}; do 
+    echo "\tCollecting "
+    for cluster in ${clusters[@]}; do
         internal_query_number=`awk "/q$i/{ print NR; exit }" cs/c${cluster}/list`
-        printf " C#$cluster(q#$internal_query_number)."
+        echo " C#$cluster(q#$internal_query_number)."
         for dm in ${div_methods[@]}; do
             query_count=`ls cs/c${cluster}/q* | wc -l`
             result_file="cqs/c${cluster}_qall${query_count}_${dm}"
-            results=`pcregrep -M "\n${internal_query_number}\tQ0\t" $result_file`
-            echo $results >> ${Q_C_MAP}_${dm}
+            echo "Looking: " $result_file
+            pcregrep -M "^${internal_query_number}\tQ0\t" $result_file | while read -r line ; do
+                echo $line >> c200_rs_cdiv_qs/q${i}/${Q_C_MAP}_${dm}
+            done
         done
-        
-        
-        
     done
-    printf "\tDone.\n"
-    
+
 done
-
-
