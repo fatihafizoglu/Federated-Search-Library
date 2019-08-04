59e76a3d958d3f31263f4579dccc2c3ca075781a
Emre Can Kucukoglu <eckucukoglu@gmail.com>
2019-08-04 15:39:32 +0200

    cdiv broker evaluates

--- utils/CDIV_broker_merge.sh
+++ utils/CDIV_broker_merge.sh
@@ -35,7 +35,7 @@ do
         for dm in ${div_methods[@]}; do
             query_count=`ls cs/c${cluster}/q* | wc -l`
             result_file="cqs/c${cluster}_qall${query_count}_${dm}"
-            # echo "Looking: " $result_file
+            #echo "Looking: " $result_file
             pcregrep -M "^${internal_query_number}\tQ0\t" $result_file >> c200_rs_cdiv_qs/q${i}/${Q_C_MAP}_${dm}
         done
     done
@@ -48,10 +48,13 @@ do
     sed -i "s/[[:digit:]]\+\tQ0/${i}\tQ0/g" c200_rs_cdiv_qs/q${i}/*
 done
 
+# TODO: Retrieve scores from main index
+
+
 # Sort results wrt their scores
 for entry in c200_rs_cdiv_qs/q*/*
 do
-    sort -r -k 5 ${entry} -o ${entry}_sorted
+    sort -rg -k5,5 ${entry} -o ${entry}_sorted
     sed -i '21,$ d' ${entry}_sorted
 done
 
@@ -69,5 +72,12 @@ done
 for rs in ${rs_methods[@]}; do
     for div in ${div_methods[@]}; do
         ./result_rank_fixer c200_rs_cdiv_results/${rs}_${div}
+    done
+done
+
+# Evaluate results
+for rs in ${rs_methods[@]}; do
+    for div in ${div_methods[@]}; do
+        ./ndeval GT_TREC09101112.txt c200_rs_cdiv_results/${rs}_${div}_fixed > c200_rs_cdiv_results/${rs}_${div}_eval
     done
 done
