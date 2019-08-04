c96e15e48d29c7f3aaf0fe17201e6cc7f0d88d79
Emre Can Kucukoglu <eckucukoglu@gmail.com>
2019-08-04 19:25:46 +0200

    resource selectino result producer fix

--- resource_selection/resource_selection.py
+++ resource_selection/resource_selection.py
@@ -175,62 +175,73 @@ class ResourceSelectionGAVG:
 
 class ResultProducer:
 
-    def __init__(self,query_id,cluster_list,query_results,result_size):
+    def __init__(self, query_id, cluster_list, query_results, inc_result_size):
 
         self.query_id = query_id
         self.cluster_list = cluster_list
         self.query_results = query_results
-        self.result_size = result_size
+        self.inc_result_size = inc_result_size
 
-    def produce_output(self,output_file):
+    def produce_output(self, output_file, cinfo):
 
+        cluster_result_counter = {}
         result_index = 1
         for (doc_id,doc_score) in self.query_results:
             cluster_id = doc_cluster_map.get_cluster_id(doc_id)
-            if cluster_id in self.cluster_list:
+            try:
+                cluster_result_count = cluster_result_counter[cluster_id]
+            except KeyError:
+                cluster_result_counter[cluster_id] = 0
+                cluster_result_count = cluster_result_counter[cluster_id]
+
+            if (cluster_id in self.cluster_list) and cluster_result_count < self.inc_result_size:
                 doc_score = "%.6f" % doc_score
-                # print "{}\tQ0\t{}\t{}\t{}\tfs".format(self.query_id,doc_id,result_index,doc_score)
-                output_file.write("{}\tQ0\t{}\t{}\t{}\tfs\n".format(self.query_id,doc_id,result_index,doc_score))
+                if cinfo:
+                    output_file.write("{}\tQ0\t{}\t{}\t{}\tfs\t{}\n".format(self.query_id, doc_id, result_index, doc_score, cluster_id))
+                else:
+                    output_file.write("{}\tQ0\t{}\t{}\t{}\tfs\n".format(self.query_id, doc_id, result_index, doc_score))
                 result_index += 1
+                cluster_result_counter[cluster_id] += 1
+
                 if result_index > self.result_size:
                     break
 
-        no_of_documents_in_selected_clusters = 0
-        for cluster_id in self.cluster_list:
-            no_of_documents_in_selected_clusters += doc_cluster_map.get_cluster_size(cluster_id)
-        return (100.0*no_of_documents_in_selected_clusters)/doc_cluster_map.get_cluster_size('total')
-
-
 parser = argparse.ArgumentParser(description='Resource Selection Algorithms')
+# INPUT FILES
 parser.add_argument('result_1', type=argparse.FileType('r'), help='result file of queries on main index')
 parser.add_argument('result_2', type=argparse.FileType('r'), help='result file of queries on CSI')
 parser.add_argument('cluster_concat_file', type=argparse.FileType('r'), help='file of document_id cluster_id pairs')
-parser.add_argument('-method', action="store",help='resource selection method (default: Redde)',default='Redde',choices=['Redde', 'Redde.top', 'CRCSExp', 'CRCSLin', 'GAVG'])
-parser.add_argument('-no_of_resource', type=int, action="store", help='no of resource to select (default: 10)', default=10, choices=[1,3,5,10,20,30,40,50,60,70,80,90,100])
+
+# MAIN PARAMETERS
+parser.add_argument('-method', action="store",help='resource selection method.',default='Redde',
+    choices=['Redde', 'Redde.top', 'CRCSExp', 'CRCSLin', 'GAVG'])
+parser.add_argument('-no_of_CSI_result', type=int, action="store", help='no of CSI result for resource selection.', default=200,
+    choices=[50,100,150,200,250,300,350,400,450,500,550,600,650,700,750,800,850,900,950,1000])
+parser.add_argument('-no_of_resource', type=int, action="store", help='no of resource to select.', default=10,
+    choices=[1,3,5,10,20,30,40,50,60,70,80,90,100])
+parser.add_argument('-inc_result_size', type=int, action="store", help='no of results from each chosen cluster.',default=10,
+    choices=[10,20,100])
+parser.add_argument('-cinfo', type=bool, action="store", help='append cluster info to results file',default=False)
+
+# ALGORITHM CONSTANTS
 parser.add_argument('-crcs_alpha', type=float, action="store", help='alpha component of CRCS Exp Formula (default:1.2)',default=1.2)
 parser.add_argument('-crcs_beta', type=float, action="store", help='beta component of CRCS Exp Formula (default:2.8)',default=2.8)
 parser.add_argument('-gavg_k', type=int, action="store", help='k component of GAVG (default:5)',default=5)
-
-parser.add_argument('-no_of_CSI_result', type=int, action="store", help='no of CSI result for resource selection (default:100)', default=200, choices=[50,100,150,200,250,300,350,400,450,500,550,600,650,700,750,800,850,900,950,1000])
-parser.add_argument('-result_size', type=int, action="store", help='no of result page desired (default: 20)', default=20, choices=[10,20,30,40,50,60,70,80,90,100,200])
 ARGUMENTS = parser.parse_args()
 
-
 doc_cluster_map = DocClusterMap(ARGUMENTS.cluster_concat_file)
-
-query_results_on_original_index    = QueryResults(ARGUMENTS.result_1)
-query_results_on_CSI            = QueryResults(ARGUMENTS.result_2)
+query_results_on_original_index = QueryResults(ARGUMENTS.result_1)
+query_results_on_CSI = QueryResults(ARGUMENTS.result_2)
 
 if query_results_on_original_index.get_query_count() != query_results_on_CSI.get_query_count():
     print "Query Counts Does Not Match!"
 
-#percentage_of_documents_in_selected_clusters_total = 0
 filename = ARGUMENTS.result_2.name + "_" + ARGUMENTS.method
-#output_file = open(filename,"w")
-output_file_clusters = open(filename + "_clusters","w")
+output_file = open(filename, "w")
+
 for query_index in range(query_results_on_original_index.get_query_count()):
 
-    top_k_results_on_CSI = query_results_on_CSI.get_results(query_index+1,ARGUMENTS.no_of_CSI_result)
+    top_k_results_on_CSI = query_results_on_CSI.get_results(query_index+1, ARGUMENTS.no_of_CSI_result)
 
     if ARGUMENTS.method == 'Redde':
         resource_selection_mode = ResourceSelectionRedde(top_k_results_on_CSI,doc_cluster_map)
@@ -246,15 +257,10 @@ for query_index in range(query_results_on_original_index.get_query_count()):
         print 'Requested resource selection method is not available'
 
     cluster_list = resource_selection_mode.get_top_k_clusters(ARGUMENTS.no_of_resource)
-    for cluster_id in cluster_list:
-        output_file_clusters.write("{} {}\n".format(query_index+1, cluster_id))
-    # cluster_list = range(100)
-    # print cluster_list
-    # break
-    #query_results = query_results_on_original_index.get_results(query_index+1,-1)
 
-    #result_producer =  ResultProducer(query_index+1,cluster_list,query_results,ARGUMENTS.result_size)
-    #percentage_of_documents_in_selected_clusters = result_producer.produce_output(output_file)
-    #percentage_of_documents_in_selected_clusters_total += percentage_of_documents_in_selected_clusters
-#output_file.close()
-#print percentage_of_documents_in_selected_clusters_total / query_results_on_original_index.get_query_count()
+    query_results = query_results_on_original_index.get_results(query_index+1,-1)
+
+    result_producer =  ResultProducer(query_index+1, cluster_list, query_results, ARGUMENTS.inc_result_size)
+    result_producer.produce_output(output_file, ARGUMENTS.cinfo)
+
+output_file.close()
